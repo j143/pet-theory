8cfcb84b9c03b5e0810fc273bef47de8be3cdb8f
j143 <j143@protonmail.com>
2021-04-17 04:11:14 +0530

    add timeslots

--- lab02/public/appointments.js
+++ lab02/public/appointments.js
@@ -17,3 +17,44 @@ firebase.auth().onAuthStateChanged(function(newUser) {
     });
   }
 });
+
+let user;
+
+firebase.auth().onAuthStateChanged(function(newUser) {
+  user = newUser;
+  if (user) {
+    const db = firebase.firestore();
+    const appColl = db.collection('customers').doc(user.email).collection('appointments');
+    appColl.orderBy('time').onSnapshot(function(snapshot) {
+      const div = document.getElementById('appointments');
+      div.innerHTML = '';
+      snapshot.docs.forEach(appointment => {
+        div.innerHTML += formatDate(appointment.data().time) + '<br/>';
+      })
+      if (div.innerHTML == '') {
+        div.innerHTML = 'No appointments scheduled';
+      }
+    });
+  }
+});
+
+const timeslots = document.getElementById('timeslots');
+
+// list open timeslots to select from
+getOpenTimes().forEach(time => {
+  timeslots.add(new Option(formatDate(time), time));
+});
+
+function getOpenTimes() {
+  const retVal = [];
+  let startDate = new Date();
+  startDate.setMinutes(0);
+  startDate.setSeconds(0);
+  startDate.setMilliseconds(0);
+  let millis = startDate.getTime();
+  while (retVal.length < 5) {
+    const hours = Math.floor(Math.random() * 5) + 1;
+    millis += hours * 3600 * 1000;
+  }
+  return retVal;
+}
