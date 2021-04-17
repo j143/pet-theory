3ae31ab6035e3002b4789755391acbc66ce7f1c0
j143 <j143@protonmail.com>
2021-04-17 15:31:35 +0530

    firestore database access

--- lab06/firebase-rest-api/solution-01/index.js
+++ lab06/firebase-rest-api/solution-01/index.js
@@ -15,3 +15,24 @@ app.get('/', async (req, res) => {
   res.json({status: 'Netflix Dataset! Make a query.'});
 
 })
+
+//--------------------------------------------------------------
+// Firestore Database Access
+// Year query
+app.get('/:year', async (req, res) => {
+  const year = req.params.year;
+  
+  const query = db.collection('data').where('release_year', '==', year);
+  const querySnapshot = await query.get();
+  if (querySnapshot.size > 0) {
+    let docs = querySnapshot.docs.map(doc => doc.data());
+
+    // Enable Cors
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");	  
+    res.json({netflix: year, content: docs});
+  }
+  else {
+    res.json({status: 'Not found'});
+  }
+})
